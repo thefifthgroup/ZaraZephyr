1b8a9c2412086b45eb40438fcd2fc0ca7f19e8c6
ken <119792185+devken0@users.noreply.github.com>
2024-01-04 02:45:10 +0800

    Add basic animation and camera tweaks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,26 +6,59 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private float aX = 0f;
+    [SerializeField] private float movementspeed;
+    [SerializeField] private float jumpStrength;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
         // Input.GetAxisRaw makes the character stop immediately after player Input
-        float aX = Input.GetAxisRaw("Horizontal");
+        aX = Input.GetAxisRaw("Horizontal");
 
         // Player's horizontal movement
-        rb.velocity = new Vector2(aX * speed, rb.velocity.y);
+        rb.velocity = new Vector2(aX * movementspeed, rb.velocity.y);
 
         // Player jump
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2 (rb.velocity.x, 14f);
+            rb.velocity = new Vector2 (rb.velocity.x, jumpStrength);
         }
+
+        UpdateAnimationState();
+
+    }
+    private void UpdateAnimationState()
+    {
+        // Player run
+        // if aX is 0 then player is idle
+
+        // run animation to the right 
+        if (aX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        // run animation to the left
+        else if (aX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        // else, player is idle
+        else
+        {
+            anim.SetBool("running", false);
+        }
+
     }
 }
