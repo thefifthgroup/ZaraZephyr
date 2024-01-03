2c8cd0d43755bd630a3c847088d76ee9df7ff5b8
ken <119792185+devken0@users.noreply.github.com>
2024-01-04 00:37:20 +0800

    Tilemap and tile palette

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Initial test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+}
