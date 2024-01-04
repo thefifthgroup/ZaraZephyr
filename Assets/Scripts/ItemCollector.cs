5e99e9d108db8acebee7686dc676f388df785562
ken <119792185+devken0@users.noreply.github.com>
2024-01-04 22:21:16 +0800

    Fix idle, run, jump, and fall animations

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,25 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpStrength;
+    [SerializeField] private LayerMask jumpFloor;
+
     private Rigidbody2D rb;
+    private BoxCollider2D boxc;
     private Animator anim;
     private SpriteRenderer sprite;
     private float aX = 0f;
-    [SerializeField] private float movementspeed;
-    [SerializeField] private float jumpStrength;
+
+    // setting a variable that contains only the four player animation/movement states
+    private enum AnimationState { idle, run, jump, fall }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        boxc = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -26,10 +33,10 @@ public class PlayerMovement : MonoBehaviour
         aX = Input.GetAxisRaw("Horizontal");
 
         // Player's horizontal movement
-        rb.velocity = new Vector2(aX * movementspeed, rb.velocity.y);
+        rb.velocity = new Vector2(aX * movementSpeed, rb.velocity.y);
 
         // Player jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2 (rb.velocity.x, jumpStrength);
         }
@@ -39,26 +46,43 @@ public class PlayerMovement : MonoBehaviour
     }
     private void UpdateAnimationState()
     {
+        AnimationState state;
         // Player run
         // if aX is 0 then player is idle
 
         // run animation to the right 
         if (aX > 0f)
         {
-            anim.SetBool("running", true);
+            state = AnimationState.run;
             sprite.flipX = false;
         }
         // run animation to the left
         else if (aX < 0f)
         {
-            anim.SetBool("running", true);
+            state = AnimationState.run;
             sprite.flipX = true;
         }
         // else, player is idle
         else
         {
-            anim.SetBool("running", false);
+            state = AnimationState.idle; 
         }
-
+        
+        // check for player jumping
+        if (rb.velocity.y > .1f)
+        {
+            state = AnimationState.jump;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = AnimationState.fall;
+        }
+        anim.SetInteger("animState", (int)state);
+    }
+    
+    private bool IsGrounded()
+    {
+        // checks if the player is overlapping the terrain/ground
+        return Physics2D.BoxCast(boxc.bounds.center, boxc.bounds.size, 0f, Vector2.down, .1f, jumpFloor);
     }
 }
