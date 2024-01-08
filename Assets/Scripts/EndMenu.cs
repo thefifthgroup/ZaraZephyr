6e7ba1551ab42ff702d9f5d7a8c4b549ed1ef50f
iyeyn <155826864+iyeyn@users.noreply.github.com>
2024-01-09 00:00:27 +0800

    Update EndMenu.cs

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -1,1 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class EndMenu : MonoBehaviour
+{
+  public void Quit()
+  {
+    Application.Quit();
+  }
+}
