136b3c7e1bdbde7a1ce0a2b3c9a7bd094e558b07
iyeyn <155826864+iyeyn@users.noreply.github.com>
2024-01-08 23:54:45 +0800

    Update Finish.cs

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,1 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Finish : MonoBehaviour
+{
+  private AudioSource finishSound;
+
+  private bool levelCompleted = false;
+  
+  private void Start()
+  {
+    finishSound = GetComponent<AudioSource>();
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.name == "Player" && !levelCompleted)
+    {
+      finishSound.Play();
+      levelCompleted = true;
+      Invoke("CompleteLevel", 2f);
+    }
+  }
+
+  private void CompleteLevel()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+}
